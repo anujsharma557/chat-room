24f20fb025dc618f74ee4ac07decbdc9c5b0ff92
Anuj <sharma.anuj557@gmail,.com>
2023-04-07 15:59:10 +0530

    initial commit for chat room application

---
+++ src/main/java/org/eterrain/ChatRoomService.java
@@ -0,0 +1,12 @@
+package org.eterrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomService
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRoomService.class, args);
+    }
+}
